50691e9c4dc677553f4fb6fb6fde6ca613ed9988
Suraj Shram <snnsun20@gmail.com>
2013-09-09 20:34:47 -0700

    Login View Updated

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,7 @@
 <div  id="login">
 <span style="color:red"><?php echo $this->session->flashdata('error');  ?></span></br></br>
 <span style="text-align:center;">User Login</span></br></br>
-<form  name="loginform"  action="<?php  echo  site_url('login/home'); ?>"  id="loginform"  method="post"  onsubmit="return check();">
+<form  name="loginform"  action="<?php  echo  site_url('login/home'); ?>"  id="loginform"  method="post">
 <label>Usernane</label>&nbsp;&nbsp;&nbsp;<label><input type="text"  name="username"  id="username"   /></label></br></br>
 
 <label>Password</label>&nbsp;&nbsp;<label><input type="password"  name="password"  id="pass"   /></label></br></br>
